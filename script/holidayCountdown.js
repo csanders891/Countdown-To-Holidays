5c9c8c733eb1ba7bd2aebda9a5ef2863a9640b81
Carter Sanders <65356624+csanders891@users.noreply.github.com>
2026-01-01 14:32:29 -0600

    Add files via upload

--- script/holidayCountdown.js
+++ script/holidayCountdown.js
@@ -5,23 +5,24 @@ var today = new Date();
 var ms = 86400000;
 
 //holidays 
-var mlkDay = new Date("January 20, 2025");
-var valentinesDay = new Date("February 14, 2025");
-var presidentsDay = new Date("February 17, 2025");
-var stPatricksDay = new Date("March 17, 2025");
-var easter = new Date("April 20, 2025");
-var mothersDay = new Date("May 11, 2025");
-var memorialDay = new Date("May 26, 2025");
-var fathersDay = new Date("June 15, 2025");
-var juneteenth = new Date("June 19, 2025");
-var fourthOfJuly = new Date("July 4, 2025");
-var laborDay = new Date("September 1, 2025");
-var columbusDay = new Date("October 13, 2025");
-var halloween = new Date("October 31, 2025");
-var veteransDay = new Date("November 11, 2025");
-var thanksgiving = new Date("November 27, 2025");
-var christmas = new Date("December 25, 2025");
-var newYears = new Date("January 1, 2026");
+var mlkDay = new Date("January 19, 2026");
+var valentinesDay = new Date("February 14, 2026");
+var presidentsDay  = new Date("February 16, 2026");
+var stPatricksDay = new Date("March 17, 2026");
+var easter = new Date("April 5, 2026");
+var mothersDay = new Date("May 10, 2026");
+var memorialDay = new Date("May 25, 2026");
+var juneteenth = new Date("June 19, 2026");
+var fathersDay = new Date("June 21, 2026");
+var fourthOfJuly = new Date("July 4, 2026");
+var laborDay = new Date("September 7, 2026");
+var columbusDay = new Date("October 12, 2026");
+var halloween = new Date("October 31, 2026");
+var veteransDay = new Date("November 11, 2026");
+var thanksgiving = new Date("November 26, 2026");
+var christmas = new Date("December 25, 2026");
+var newYears = new Date("January 1, 2027");
+
 
 
 //holiday countdown  
@@ -32,8 +33,8 @@ var stPatricksDayCountdown = (stPatricksDay - today) / ms;
 var easterCountdown = (easter - today) / ms;
 var mothersDayCountdown = (mothersDay - today) / ms;
 var memorialDayCountdown = (memorialDay - today) / ms;
-var fathersDayCountdown = (fathersDay - today) / ms;
 var juneteenthCountdown = (juneteenth - today) / ms;
+var fathersDayCountdown = (fathersDay - today) / ms;
 var fourthOfJulyCountdown = (fourthOfJuly - today) / ms;
 var laborDayCountdown = (laborDay - today) / ms;
 var columbusDayCountdown = (columbusDay - today) / ms;
@@ -43,6 +44,7 @@ var thanksgivingCountdown = (thanksgiving - today) / ms;
 var christmasCountdown = (christmas - today) / ms;
 var newYearsCountdown = (newYears - today) / ms;
 
+
 //remaining days rounded to the nearest whole number
 mlkCountdown = Math.round(mlkCountdown);
 valentinesDayCountdown = Math.round(valentinesDayCountdown);
@@ -51,8 +53,8 @@ stPatricksDayCountdown = Math.round(stPatricksDayCountdown);
 easterCountdown = Math.round(easterCountdown);
 mothersDayCountdown = Math.round(mothersDayCountdown);
 memorialDayCountdown = Math.round(memorialDayCountdown);
-fathersDayCountdown = Math.round(fathersDayCountdown);
 juneteenthCountdown = Math.round(juneteenthCountdown);
+fathersDayCountdown = Math.round(fathersDayCountdown);
 fourthOfJulyCountdown = Math.round(fourthOfJulyCountdown);
 laborDayCountdown = Math.round(laborDayCountdown);
 columbusDayCountdown = Math.round(columbusDayCountdown);
@@ -64,9 +66,9 @@ newYearsCountdown = Math.round(newYearsCountdown);
 
 
 
-var holidayCountdown = [mlkCountdown, valentinesDayCountdown, presidentsDayCountdown, stPatricksDayCountdown, easterCountdown, mothersDayCountdown, memorialDayCountdown, fathersDayCountdown, juneteenthCountdown, fourthOfJulyCountdown, laborDayCountdown, columbusDayCountdown, halloweenCountdown, veteransDayCountdown, thanksgivingCountdown, christmasCountdown, newYearsCountdown];
+var holidayCountdown = [mlkCountdown, valentinesDayCountdown, presidentsDayCountdown, stPatricksDayCountdown, easterCountdown, mothersDayCountdown, memorialDayCountdown, juneteenthCountdown, fathersDayCountdown, fourthOfJulyCountdown, laborDayCountdown, columbusDayCountdown, halloweenCountdown, veteransDayCountdown, thanksgivingCountdown, christmasCountdown, newYearsCountdown];
 
-var holidays = ["King's Birthday", "Valentine's Day", "President's Day", "St. Patrick's Day", "Easter", "Mother's Day", "Memorial Day", "Father's Day", "Juneteenth", "the 4th of July", "Labor Day", "Columbus Day", "Halloween", "Veteran's Day", "Thanksgiving", "Christmas", "New Year's Day"];
+var holidays = ["King's Birthday", "Valentine's Day", "President's Day", "St. Patrick's Day", "Easter", "Mother's Day", "Memorial Day",  "Juneteenth", "Father's Day", "the 4th of July", "Labor Day", "Columbus Day", "Halloween", "Veteran's Day", "Thanksgiving", "Christmas", "New Year's Day"];
 
 
 for(var i =0; i < holidays.length; i++)
